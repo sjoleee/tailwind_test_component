2e42a83b8842d7746a309a57cdafbb12c07c9101
sjoleee <cent7425@gmail.com>
2023-11-10 00:31:03 +0900

    delete

--- dist/index.d.mts
+++
@@ -1,8 +0,0 @@
-import * as react_jsx_runtime from 'react/jsx-runtime';
-
-type Props = {
-    name?: string;
-};
-declare const Init: ({ name }: Props) => react_jsx_runtime.JSX.Element;
-
-export { Init };
--- dist/index.mjs
+++
@@ -1,9 +0,0 @@
-// src/Init.tsx
-import { jsx } from "react/jsx-runtime";
-var Init = ({ name }) => {
-  return /* @__PURE__ */ jsx("div", { className: "bg-gray-500", children: name });
-};
-export {
-  Init
-};
-//# sourceMappingURL=index.mjs.map
